625256c19923bb169868d3ec59ac233e64a6f146
pierresousa <pierrevssousa@gmail.com>
2020-07-13 16:05:01 -0300

    pagina criando pergunta

--- src/pages/CriandoPergunta.vue
+++ src/pages/CriandoPergunta.vue
@@ -1,7 +1,284 @@
 <template>
   <q-page padding>
     <!-- content -->
-    Criando pergunta
+    <h4 class="text-color-black text-weight-bold">Criando pergunta</h4>
+    <div class="text-center">
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_01.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 1 - <i>Botão mais para incluir nova pergunta ao grupo</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_02.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 2 - <i>Ferramentas para edição da pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_03.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 3 - <i>Botão edição HTML para acessibilidade</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_04_0.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 4_0 - <i>Código auxiliar na tag Div</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_04_1.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 4_1 - <i>Incluir video para auxílio em libras</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_04_2.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 4_2 - <i>Imagem com link para video de tradução em libras</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_05.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 5 - <i>Botão de seleção do tipo de pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_06.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 6 - <i>Opções de tipo de pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_07.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 7 - <i>Tipo de questão de resposta única selecionada</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_08.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 8 - <i>Opção escolhida e botão de ativação</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_09.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 9 -<i> Opção escolhida, lista de menu dropdown</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_10.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 10 - <i>Botão para definir se pergunta obrigatória</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_11.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 11 - <i>Formas de salvar</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_12.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 12 - <i>Alerta para criaçãode respostas para a pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_13.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 13 - <i>Crie perguntas pelo link ou botão</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_14.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 14 - <i>Código e texto da resposta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_15.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 15 - <i>Como criar o código da resposta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_16.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 16 - <i>Sugestão para identificar a primeira resposta da lista</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_17_0.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 17_0 - <i>Botão que abre mais opção de edição da resposta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_17_1.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 17_1 - <i>Ferramentas para edição e formatação do texto da resposta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_18.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 18 - <i>Botão para inserir nova resposta a pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_19.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 19 - <i>Código padrão</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_20.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 20 - <i>Texto da segunda resposta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_21_0.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 21_0 - <i>Fornas de salvar</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_21_1.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 21_1 - <i>Botão para visualização prévia da pergunta criada</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_21_2.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 21_2 - <i>Visualização das respostas para a pergunta criada</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_21_3.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 21_3 - <i>Listar grupos de perguntas e incluir nova pergunta a um grupo</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_22.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 22 - <i>Criando uma nova pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_23.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 23 - <i>Tipo da questão</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_24.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 24 - <i>Formato escolhido para a pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_25.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 25 - <i>Selecione obrigatória para que a pergunta seja obrigatória</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_26.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 26 - <i>Caso seja necessário oferecer outras opções como resposta selecione Outro ligado</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_27_0.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 27_0 - <i>Abra o menu Mostrar para personalizar o texto da opção outros</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_27_1.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 27_1 - <i>Botão salvar para gravar alterações e iniciar edição das respostas</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_28_0.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 28_0 - <i>Adicionar opções de resposta pelo link ou botão</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_28_1.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 28_1 - <i>Respostas no padrão sugerido</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_29.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 29 - <i>Após finalizar a edição, vá em pré-visualizar para avaliar </i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_30_0.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 30_0 - <i>Visualização das respostas para a pergunta criada</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_30_1.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 30_1 - <i>Listar grupos e adicionar perguntas a um grupo</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_31.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 31 - <i>Criando uma nova pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_32.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 32 - <i>Tipo da questão</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_33.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 33 - <i>Formato escolhido e botao para tornar obrigatória a questão</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_34.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 34 - <i>Acesse o link ou o botão para editar as perguntas</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_35.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 35 - <i>Textos das respostas e códigos no padrão sugerido</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_36.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 36 - <i>Formas de salvar</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_37.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 37 - <i>Botão pré-visualizar para visualização prévia da pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_38.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 38 - <i>Visualização da pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_39.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 39 - <i>Listar grupos e adicionar perguntas a um grupo</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_40.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 40 - <i>Criando uma nova pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_41.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 41 - <i>Tipo da questão</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_42.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 42 - <i>Tipo escolhido</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_43.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 43 - <i>Opção de resposta obrigatória desligado</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_44.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 44 - <i>Formas de salvar</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_45.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 45 - <i>Botão pré-visualizar para visualização prévia da pergunta</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_pergunta/criando_pergunta_46.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 46 - <i>Visualização da pergunta</i></p>
+    </div>
   </q-page>
 </template>
 
