17e2aa9f5a75819b62cdabf785a5519276cceed4
pierresousa <pierrevssousa@gmail.com>
2020-07-13 15:06:08 -0300

    pagina criando grupo

--- src/pages/CriandoGrupo.vue
+++ src/pages/CriandoGrupo.vue
@@ -1,7 +1,64 @@
 <template>
   <q-page padding>
     <!-- content -->
-    Criando grupo
+    <h4 class="text-color-black text-weight-bold">Criando grupo</h4>
+    <div class="text-center">
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_01.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 1 - <i>Identifique listar grupos de perguntas</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_02.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 2 - <i>Clique em listar grupos de perguntas</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_03.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 3 - <i>Clique em adicionar um grupo de perguntas se necessário</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_04.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 4 - <i>De um nome e descrição para o grupo</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_05.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 5 - <i>Ferramentar para formatação de texto</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_06.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 6 - <i>Botão de edição HTML para auxíliar na acessibilidade</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_07.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 7 - <i>No modo HTML, é possível a insersão de código</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_08.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 8 - <i>Código auxiliar na tag Div</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_09.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 9 - <i>Opções de salvar</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_10.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 10 - <i>Resumo do grupo</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/criando_grupo/criando_grupo_11.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 11 - <i>Listagem dos grupos em ordem de criação</i></p>
+    </div>
   </q-page>
 </template>
 
