d9b5b98d601528baceb8ef99c5efb7023ad766d7
pierresousa <pierrevssousa@gmail.com>
2020-07-13 12:13:04 -0300

    Duas paginas concluidas: criar questionario e configurando apresentação

--- src/pages/ConfigurandoApresentacao.vue
+++ src/pages/ConfigurandoApresentacao.vue
@@ -1,7 +1,29 @@
 <template>
   <q-page padding>
     <!-- content -->
-    Configurando apresentacao
+    <h4 class="text-color-black text-weight-bold">Configurando apresentação</h4>
+    <div class="text-center">
+      <q-img
+        src="manual_limesurvey_cecom/configuracao_apresentacao/configuracao_apresentacao_01.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 1 - <i>Opções de exibição</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/configuracao_apresentacao/configuracao_apresentacao_02.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 2 - <i>Permitir navegação inversa</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/configuracao_apresentacao/configuracao_apresentacao_03.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 3 - <i>Imprimir respostas e visualizar estatística</i></p>
+      <q-img
+        src="manual_limesurvey_cecom/configuracao_apresentacao/configuracao_apresentacao_04.png"
+        style="widtg: 100%"
+      />
+      <p>Figura 4 - <i>URL final do questionário</i></p>
+    </div>
   </q-page>
 </template>
 
